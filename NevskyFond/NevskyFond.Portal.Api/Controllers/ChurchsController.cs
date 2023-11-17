ae4fbd6967c7ba4d83e9b69ba2aebc215f8f93b8
Andrew <Andrew@DESKTOP-2R081NN>
2023-11-17 16:30:56 +0300

    Добавьте файлы проекта.

---
+++ NevskyFond/NevskyFond.Portal.Api/Controllers/ChurchsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NevskyFond.Portal.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ChurchsController : ControllerBase
+    {
+        public ChurchsController()
+        {
+                
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddChurch()
+        {
+            return Ok(DateTime.Now);
+        }
+    }
+}
